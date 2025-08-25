3cc7bc0d0aba5ae7e3b0d489802cda9ba170847e
ilyasbural <ilyasbural@gmail.com>
2025-08-25 17:43:38 +0300

    avipes docker project

---
+++ DockerAspnetCore.Avipes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockerAspnetCore.Avipes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
